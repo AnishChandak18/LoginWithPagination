07c86d54d0b42308f5d86451d8fa02fb1d567a8f
rahulZTK <rahul.koli@zartek.in>
2022-12-27 12:41:31 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use("/", (req, res, next) => {
       jwt.verify(req.headers.token, 'shhhhh11111', function (err, decoded) {
         if (decoded && decoded.user) {
           req.user = decoded;
+          console.log(req.user)
           next();
         } else {
           return res.status(401).json({
